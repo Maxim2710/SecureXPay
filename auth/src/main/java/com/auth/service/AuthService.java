fabf6850cdbae494c822f50abba8d95a64d1e964
PrimakMV <pm2710@mail.ru>
2025-01-10 21:44:50 +0300

    [SP-B-J-003] Implemented logic for user authorization

--- auth/src/main/java/com/auth/service/AuthService.java
+++ auth/src/main/java/com/auth/service/AuthService.java
@@ -2,7 +2,9 @@ package com.auth.service;
 
 import com.auth.bom.UserBom;
 import com.auth.converter.UserConverter;
+import com.auth.dto.AccountLoginForm;
 import com.auth.dto.AccountRegistrationForm;
+import com.auth.dto.AccountResponseLogin;
 import com.auth.dto.AccountResponseRegister;
 import com.auth.model.role.Role;
 import com.auth.model.user.User;
@@ -24,6 +26,7 @@ public class AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Transactional
     public AccountResponseRegister registerUser(AccountRegistrationForm registrationForm) {
         if (userRepository.existsByEmail(registrationForm.getEmail())) {
             throw new RuntimeException("Email уже занят");
@@ -44,5 +47,19 @@ public class AuthService {
 
         return new AccountResponseRegister("Регистрация успешна", token, userBom);
 
+    }
+
+    public AccountResponseLogin loginUser(AccountLoginForm accountLoginForm) {
+        User user = userRepository.findByEmail(accountLoginForm.getEmail())
+                .orElseThrow(() -> new RuntimeException("Пользователь с таким email не найден"));
+
+        if (!passwordEncoder.matches(accountLoginForm.getPassword(), user.getHashedPassword())) {
+            throw new RuntimeException("Неверный пароль");
+        }
+
+        String token = jwtUtils.generateToken(user.getEmail());
+        UserBom userBom = UserConverter.convertToBom(user);
+
+        return new AccountResponseLogin("Авторизация успешна", token, userBom);
     }
 }
