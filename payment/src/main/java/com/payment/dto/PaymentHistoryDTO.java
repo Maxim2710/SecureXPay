feda99214bb0120754e2eb39e48f3c0599e7903d
PrimakMV <pm2710@mail.ru>
2025-01-12 16:57:00 +0300

    [SP-B-J-011] A search method has been added to the repository, and a model has been created to generate a response with a history of operations.

---
+++ payment/src/main/java/com/payment/dto/PaymentHistoryDTO.java
@@ -0,0 +1,18 @@
+package com.payment.dto;
+
+import com.payment.model.status.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class PaymentHistoryDTO {
+    private Long id;
+    private BigDecimal amount;
+    private PaymentStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
